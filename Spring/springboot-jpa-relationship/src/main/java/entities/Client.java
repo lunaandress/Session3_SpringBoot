30cc75e3541155de17c806903629503b5e998a1c
Andres Luna <miosesfreddy123@gmail.com>
2026-03-18 12:29:49 +0100

    .Nuevo Proyecto Client and Invoice( La relación ManyToOne)

---
+++ Spring/springboot-jpa-relationship/src/main/java/entities/Client.java
@@ -0,0 +1,59 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+
+    private String name;
+    private String lastname;
+
+
+
+
+    public Client(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    
+    public Client() {
+        
+    }
+
+
+
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    
+
+}
---
+++ Spring/springboot-jpa-relationship/src/main/java/entities/Invocie.java
@@ -0,0 +1,69 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="invoice")
+public class Invocie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+
+    private String description;
+    private long total ;
+
+    //relaciones
+    @ManyToOne
+    private Client client;
+
+
+
+    public Invocie(String description, long total) {
+        this.description = description;
+        this.total = total;
+    }
+    public Invocie() {
+
+        
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public long getTotal() {
+        return total;
+    }
+    public void setTotal(long total) {
+        this.total = total;
+    }
+    @Override
+    public String toString() {
+        return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
+    }
+
+
+    
+
+
+    
+
+
+
+}

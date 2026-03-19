db032ebec7b6679a5bbde6002cb61cb14783e0a6
Andres Luna <miosesfreddy123@gmail.com>
2026-03-19 11:36:50 +0100

    Esquema con tabla de enlace en OneToMany

--- Spring/springboot-jpa-relationship/src/main/java/com/andres/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ Spring/springboot-jpa-relationship/src/main/java/com/andres/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="clients")
@@ -23,8 +25,11 @@ public class Client {
     private String name;
     private String lastname;
 
+    //@JoinColumn(name="client_id")
     @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)// se insewrta o borra en cascada osea en conjunto
-    @JoinColumn(name="client_id")
+    @JoinTable(name="tbl_clientes_to_direcciones", joinColumns = @JoinColumn(name="id_cliente"),
+    inverseJoinColumns = @JoinColumn(name="id_direcciones"),
+    uniqueConstraints = @UniqueConstraint(columnNames = {"id_direcciones"}))
     private List<Address> addressList; //lista de direcciones
 
     public Client(String name, String lastname) {

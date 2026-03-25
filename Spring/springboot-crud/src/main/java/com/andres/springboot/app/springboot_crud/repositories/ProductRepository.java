687a11028dd0bae9ab53cb9fe79216bf1535a4c9
Andres Luna <miosesfreddy123@gmail.com>
2026-03-25 17:52:27 +0100

    Bakend User Spring Security  IMPORTANTICIMO

--- Spring/springboot-crud/src/main/java/com/andres/springboot/app/springboot_crud/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.andres.springboot.app.springboot_crud.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.andres.springboot.app.springboot_crud.entities.Product;
-
-public interface ProductRepository extends CrudRepository<Product,Long>{
-
-}
